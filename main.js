1990970776f0df702a6ab827f8dc4cfcca096ac7
LAbeast021 <kiarashhesampour@gmail.com>
2024-02-20 22:34:35 -0800

    trying to implement the computer opponent

--- main.js
+++ main.js
@@ -15,17 +15,24 @@ let turn, winner, clickPattern, count, XO
 turn =  player[0];
 clickPattern = [0,0,0,0,0,0,0,0,0];
 count = 0
+playingComputer = false;
 /*----- cached element references -----*///////////////////////////////////////////////////////////////////////////////////
 let btn = document.querySelector("section");
 let square = document.querySelectorAll("section div");
 let reset = document.querySelector("button");
 let message = document.getElementById("text");
+let opponent = document.getElementById("opponentSelector");
+
 /*----- event listeners -----*//////////////////////////////////////////////////////////////////////////////////////////////
+opponent.addEventListener("click", function(evt){
+    playingComputer = true;
+})
 btn.addEventListener("click", clickHandler);
 // $("section div").hover(function(evt) {
 //     console.log(evt.target)
 // })
 reset.addEventListener("click",init);
+
 /*----- functions -----*////////////////////////////////////////////////////////////////////////////////////////////////////
 function clickHandler(evt){
     if(evt.target.textContent==="" && winner!==turn){
@@ -51,20 +58,43 @@ function clickHandler(evt){
         // console.log(target , "------" , square.indexOf(target)) 
         // clickPattern.splice(square.indexOf(target),1,turn);
         target.textContent= XO;
-        winCheck();
+        winCheck(false);
 }
 }
-function winCheck (){
+function winCheck (computerPlayed){
     for(i=0 ; i < winnPattern.length ; i++){
         if(clickPattern[winnPattern[i][0]] + clickPattern[winnPattern[i][1]] + clickPattern[winnPattern[i][2]] === -3 ) {
             winner = turn;
-    }
+        }
         else if(clickPattern[winnPattern[i][0]] + clickPattern[winnPattern[i][1]] + clickPattern[winnPattern[i][2]] === 3 ) {
              winner = turn
-    }  
+        }  
+
+    }
+
+    if(!computerPlayed){
+        render();
+    }
+
 }
-render();
+
+function computerMove(){
+    let random = Math.floor(Math.random() * 9);
+    if(square[random].textContent===""){
+        turn = player[0];
+        XO = "O"
+        square[random].style.color = "black";
+        message.innerHTML = `it is your turn player <span class="red">X</span>`;
+        square[random].className += "clicked" ;
+        count +=1;
+        clickPattern.splice(random,1,turn);
+        square[random].textContent= XO;
+        winCheck(true);
+    }
+    
 }
+
+
 function init(){
     for(i=0 ; i< clickPattern.length ; i++){
         square[i].classList.remove("clicked");
@@ -102,6 +132,10 @@ function render(){
         for(i=0 ; i < clickPattern.length ; i ++) {
             square[i].classList.contains("clicked") ? console.log("clicked") : square[i].className += "clicked" ;
         }
+    }
+
+    else if (playingComputer){
+        computerMove();
     }
 }
 
